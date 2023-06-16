c5e012158e41b70d98e594103265286c097d0785
sanjay v <vsanjay8722@gmail.com>
2023-06-16 17:55:41 +0530

    added quiz

--- react-quiz/react-quiz/src/components/QuestionCard.tsx
+++ react-quiz/react-quiz/src/components/QuestionCard.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { AnswerObject } from "../App";
 type Props = {
     question: string;
     answers: string[];
-    callback: any;
-    userAnswer: any;
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    userAnswer: AnswerObject | undefined;
     questionNr: number;
     totalQuestions: number;
 }
 const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNr, totalQuestions }) => {
+    console.log({ question, answers, callback, userAnswer, questionNr, totalQuestions })
     return (
         <div>
             <p className="number">
@@ -16,15 +18,18 @@ const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer
             <p dangerouslySetInnerHTML={{ __html: question }} />
             <div>
                 {
-                    answers.map(ans => (
-                        <div>
-                            <button disabled={userAnswer} onClick={callback}>
-                                <span dangerouslySetInnerHTML={{ __html:ans}} />
-                            </button>
-                        </div>
 
-                    ))
+                    answers.map((answer) => {
+                        return (
+                            <div key={answer}>
+                                <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+                                    <span dangerouslySetInnerHTML={{ __html: answer }} />
+                                </button>
+                            </div>
+                        )
+                    })
                 }
+
             </div>
         </div>
     )
